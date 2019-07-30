fa7cb9e32acd8db0b38e4f13db9f887830a67602
Kashmetha <kashmetha@gmail.com>
2019-07-30 17:22:53 +0530

    ssl commit

--- src/main/java/com/test/HelloWorld.java
+++ src/main/java/com/test/HelloWorld.java
@@ -1,17 +1,18 @@
 package com.test;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import java.security.SecureRandom;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
 
+        SSLContext ctx = SSLContext.getInstance("TLS");
+        ctx.init(new KeyManager[0], new TrustManager[] {new DefaultTrustManager()}, new SecureRandom());
+        SSLContext.setDefault(ctx);
 
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
     }
 }
 
