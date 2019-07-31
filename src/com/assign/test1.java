ac0415e1a4224dcd055a377050edd6448c92ee5d
Kashmetha <kashmetha@gmail.com>
2019-07-31 16:01:49 +0530

    commit

--- src/com/assign/test1.java
+++ src/com/assign/test1.java
@@ -2,7 +2,7 @@ package com.assign;
 
 class test1{
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.print("test");
         System.out.print("commit");
     }
 
