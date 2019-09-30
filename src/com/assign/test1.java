774941879f8875bc57803d1193d66538828b6b71
Kashmetha <kashmetha@gmail.com>
2019-09-30 11:33:35 +0530

    test1 print commit

--- src/com/assign/test1.java
+++ src/com/assign/test1.java
@@ -4,6 +4,10 @@ class test1{
     public static void main(String[] args) {
         System.out.println("test");
         System.out.print("commit");
+        System.out.println("test");
+        System.out.print("commit");
+        System.out.println("test");
+        System.out.print("commit");
     }
 
 }
